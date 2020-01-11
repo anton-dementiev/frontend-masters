5f994b28175e5df360d15f07d28ba208e1fc49a7
Anton <anton.dementyev@gmail.com>
2020-01-11 13:58:26 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send("Hello from Node.js running on Google Cloud Platform.This is for the FrontendMasters course .Anton Dementiev");
+});
+
+app.listen(port, ()=> console.log(`Example app listenting on port ${port}`));
