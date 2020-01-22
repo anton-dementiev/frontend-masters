fe741867726f2711c0b79de084c54271994223e5
Anton Dementiev <anton.dementyev@gmail.com>
2020-01-22 18:47:31 +0300

    another try

--- data/mysql.js
+++ data/mysql.js
@@ -13,9 +13,9 @@ con.connect(function(err){
         if (err) throw err;
         console.log('Database created');
        con.query("USE test; CREATE TABLE IF NOT EXISTS customers (" +
-       "first_name VARCHAR(55) NOT NULL" +
-        "last_name VARCHAR(55) NOT NULL" +
-        "id TINYINT NOT NULL AUTO_INCREMENT" +
+       "first_name VARCHAR(55) NOT NULL " +
+        "last_name VARCHAR(55) NOT NULL " +
+        "id TINYINT NOT NULL AUTO_INCREMENT"  +
         "PRIMARY KEY(id)" + ")", 
         function(err, result){
              if (err) throw err;
