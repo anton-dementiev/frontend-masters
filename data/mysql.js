44d8a540875e8e49cec9fd267f661553b1edc105
Anton Dementiev <anton.dementyev@gmail.com>
2020-01-22 18:49:55 +0300

    commented out mysql

--- data/mysql.js
+++ data/mysql.js
@@ -1,26 +1,26 @@
-var mysql = require('mysql');
-var con = mysql.createConnection({
-    host:'localhost',
-    user: 'Anton',
-    password: '309013abraham'
-});
+// var mysql = require('mysql');
+// var con = mysql.createConnection({
+//     host:'localhost',
+//     user: 'Anton',
+//     password: '309013abraham'
+// });
 
-con.connect(function(err){
+// con.connect(function(err){
 
-    if (err) throw err;
-    console.log('Connected to DB');
-    con.query("CREATE DATABASE IF NOT EXISTS test", function(err, result){
-        if (err) throw err;
-        console.log('Database created');
-       con.query("USE test; CREATE TABLE IF NOT EXISTS customers (" +
-       "first_name VARCHAR(55) NOT NULL " +
-        "last_name VARCHAR(55) NOT NULL " +
-        "id TINYINT NOT NULL AUTO_INCREMENT"  +
-        "PRIMARY KEY(id)" + ")", 
-        function(err, result){
-             if (err) throw err;
-             console.log(result);
-        });
-    });
+//     if (err) throw err;
+//     console.log('Connected to DB');
+//     con.query("CREATE DATABASE IF NOT EXISTS test", function(err, result){
+//         if (err) throw err;
+//         console.log('Database created');
+//        con.query("USE test; CREATE TABLE IF NOT EXISTS customers (" +
+//        "first_name VARCHAR(55) NOT NULL " +
+//         "last_name VARCHAR(55) NOT NULL " +
+//         "id TINYINT NOT NULL AUTO_INCREMENT"  +
+//         "PRIMARY KEY(id)" + ")", 
+//         function(err, result){
+//              if (err) throw err;
+//              console.log(result);
+//         });
+//     });
 
-});
+// });
